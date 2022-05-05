08962b6e3268591bf17cc42d6369f065d4d91834
Babb Getachew <babbgetachew@gmail.com>
2022-05-05 10:52:28 -0400

    Added constructor and method on prototype

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 
 $(document).ready(function(){
 
+    function Bookshelf(title, author, numPages, read)
+    {
+        this.title = title,
+        this.author = author,
+        this.numPages = numPages,
+        this.read = read;
+    }
 
+
+    //Define functions on the prototype of the object
+    Bookshelf.prototype.info = function (){
+        return (`${title}, by ${author}, ${numPages} pages ${read}`);
+    }
 
 })
